43020c937c1bb8f185c4b52ea1d3fbd6338964ed
Mariya <mariya.podosinova@gmail.com>
2020-08-20 19:26:22 +0100

    my solution

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [time, setTime] = useState();
+
+  // function curentTime() {
+  //   let time = new Date().toLocaleTimeString();
+  //   setTime(time);
+  // }
+
+  function timer() {
+    let time = new Date().toLocaleTimeString();
+    setTime(time);
+  }
+  setInterval(timer, 1000);
+
   return (
     <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
+      <h1>{time}</h1>
+      <button onClick={timer}>Get Time</button>
     </div>
   );
 }
